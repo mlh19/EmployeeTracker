b0f229900c7d61591366a669395b7660435b20dc
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-10-23 21:57:03 -0500

    Add functions for each menu option.

--- index.js
+++ index.js
@@ -2,19 +2,26 @@
 const inquirer = require("inquirer");
 
 // Properties
+const viewAllDepartmentsChoice = "View All Departments"
+const viewAllRolesChoice = "View All Roles"
+const viewAllEmployeesChoice = "View All Employees"
+const addARoleChoice = "Add a Role"
+const addAnEmployeeChoice = "Add an Employee"
+const updateEmployeeRoleChoice = "Update an Employee Role"
+
 const promptQuestion = [
     {
-        type: "checkbox",
+        type: "rawlist",
         message: "What do you want to do?",
         choices: [
-            "View All Departments",
-            "View All Roles",
-            "View All Employees",
-            "Add a Role",
-            "Add an Employee",
-            "Update an Employee Role"
+            viewAllDepartmentsChoice,
+            viewAllRolesChoice,
+            viewAllEmployeesChoice,
+            addARoleChoice,
+            addAnEmployeeChoice,
+            updateEmployeeRoleChoice
         ],
-        name: "Menu",
+        name: "menu"
     }
 ]
 
@@ -22,11 +29,49 @@ const promptQuestion = [
 function promptMenu() {
     inquirer
     .prompt(promptQuestion)
-    .then((response) =>
-    console.log(response)
+    .then((response) => 
+    console.log("The user selected " + response.menu)
+        // handlePromptResponse(response.name)
     )
     .catch((err) => console.error(err));
 }
 promptMenu();
 
+// Helper Functions
+function handlePromptResponse(choice) {
+    console.log("The user selected " + choice);
+    switch (choice) {
+        case viewAllDepartmentsChoice: viewAllDepartments();
+        case viewAllRolesChoice: viewAllRoles();
+        case viewAllEmployeesChoice: viewAllEmployees();
+        case addARoleChoice: addARole();
+        case addAnEmployeeChoice: addAnEmployee();
+        case updateEmployeeRoleChoice: updateEmployeeRole();
+        break;
+    }
+}
+
 // Menu Functions
+function viewAllDepartments() {
+    console.log(viewAllDepartmentsChoice + " Selected.");
+}
+
+function viewAllRoles() {
+    console.log(viewAllRolesChoice + " Selected.");
+}
+
+function viewAllEmployees() {
+    console.log(viewAllEmployeesChoice + " Selected.");
+}
+
+function addARole() {
+    console.log(addARoleChoice + " Selected.");
+}
+
+function addAnEmployee() {
+    console.log(addAnEmployeeChoice + " Selected.");
+}
+
+function updateEmployeeRole() {
+    console.log(updateEmployeeRoleChoice + " Selected.");
+}
