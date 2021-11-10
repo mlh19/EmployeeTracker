076ec94e1cbefc0b34388771d482b041871ec13b
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-11-10 01:09:05 -0600

    fix syntax errors, and debug code.

--- db/seed.sql
+++ db/seed.sql
@@ -8,13 +8,13 @@ Values ("Numerical Analyst", 2250000, 1),
 ("Pure Mathematician", 3000000, 1),
 ("Electrical Engineer", 1500000, 2),
 ("Civil Engineer", 2500000, 2),
-("Theoretical CS", 5000000)
+("Theoretical CS", 5000000, 3),
 ("iOS Developer", 7365000, 3);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 Values("Bernhard", "Riemann", 1, null), 
 ("Johann Carl", "Gauss", 2, 1),
 ("Nikola", "Tesla", 3, 1),
-("Leonardo", "daVinci", 4, 1)
-("Alan", "Turing", 5, 1);
-("Chris", "Lattner", 6, 1)
+("Leonardo", "daVinci", 4, 1),
+("Alan", "Turing", 5, 1),
+("Chris", "Lattner", 6, 1);
--- index.js
+++ index.js
@@ -5,205 +5,204 @@ const { waitForDebugger } = require("inspector");
 const mysql = require('mysql2');
 const { start } = require("repl");
 const cTable = require("console.table");
+const util = require("util");
 // Run 'brew services start mysql' to start the local MySQL server.
 // Run 'brew services stop mysql' to end the connection.
-var sqlConnection;
 const host = "127.0.0.1";
 const port = "3306";
 const username = "root";
 const database = "employeetracker_db";
 
-// - Properties
-var isDebug = true;
-const viewAllDepartmentsChoice = "View All Departments";
-const viewAllRolesChoice = "View All Roles";
-const viewAllEmployeesChoice = "View All Employees";
-const addADepartmentChoice = "Add a Department";
-const addARoleChoice = "Add a Role";
-const addAnEmployeeChoice = "Add an Employee";
-const updateEmployeeRoleChoice = "Update an Employee Role";
-const promptQuestion = [
-    {
-        type: "rawlist",
-        message: "What do you want to do?",
-        choices: [
-            viewAllDepartmentsChoice,
-            viewAllRolesChoice,
-            viewAllEmployeesChoice,
-            addADepartmentChoice,
-            addARoleChoice,
-            addAnEmployeeChoice,
-            updateEmployeeRoleChoice
-        ],
-        name: "menu"
+const sqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Idasftw1',
+    database: "employeetracker_db"
+});
+sqlConnection.connect(err => {
+    if (err) {
+        logMessage(err);
+    } else {
+        logMessage("MySQL connection started.");
+        promptMenu();
+
+}});
+    const query = util.promisify(sqlConnection.query).bind(sqlConnection);
+
+
+    // - Properties
+    var isDebug = true;
+    const viewAllDepartmentsChoice = "View All Departments";
+    const viewAllRolesChoice = "View All Roles";
+    const viewAllEmployeesChoice = "View All Employees";
+    const addADepartmentChoice = "Add a Department";
+    const addARoleChoice = "Add a Role";
+    const addAnEmployeeChoice = "Add an Employee";
+    const updateEmployeeRoleChoice = "Update an Employee Role";
+    const promptQuestion = [
+        {
+            type: "rawlist",
+            message: "What do you want to do?",
+            choices: [
+                viewAllDepartmentsChoice,
+                viewAllRolesChoice,
+                viewAllEmployeesChoice,
+                addADepartmentChoice,
+                addARoleChoice,
+                addAnEmployeeChoice,
+                updateEmployeeRoleChoice
+            ],
+            name: "menu"
+        }
+    ]
+    // name, salary, and department
+    const addARoleQuestions = [
+        { type: "input", name: "role", message: "Enter New Role: " },
+        { type: "input", name: "salary", message: "Enter Salary: $" },
+        { type: "input", name: "department", message: "Enter Department to Add To: " }
+    ]
+
+    // first name, last name, role, manager
+    const addAnEmployeeQuestions = [
+        { type: "input", name: "firstName", message: "Enter the first name: " },
+        { type: "input", name: "lastName", message: "Enter the last name: " },
+        { type: "input", name: "role", message: "Enter the role: " },
+        { type: "input", name: "manager", message: "Enter their manager's first name: " }
+    ]
+
+
+    // - Menu
+    function promptMenu() {
+        // This function is called after a successful SQL connection.
+        inquirer
+            .prompt(promptQuestion)
+            .then((response) => handlePromptResponse(response.menu))
+            .catch((err) => console.error("There was an error: " + err));
     }
-]
-// name, salary, and department
-const addARoleQuestions = [
-    { type: "input", name: "role", message: "Enter New Role: " },
-    { type: "input", name: "salary", message: "Enter Salary: $" },
-    { type: "input", name: "department", message: "Enter Department to Add To: " }
-]
 
-// first name, last name, role, manager
-const addAnEmployeeQuestions = [
-    { type: "input", name: "firstName", message: "Enter the first name: " },
-    { type: "input", name: "lastName", message: "Enter the last name: " },
-    { type: "input", name: "role", message: "Enter the role: " },
-    { type: "input", name: "manager", message: "Enter their manager's first name: " }
-]
-
-
-// - Menu
-function promptMenu() {
-    // This function is called after a successful SQL connection.
-    inquirer
-        .prompt(promptQuestion)
-        .then((response) => handlePromptResponse(response.menu))
-        .catch((err) => console.error("There was an error: " + err));
-}
-
-function handlePromptResponse(choice) {
-    switch (choice) {
-        case viewAllDepartmentsChoice:
-            viewAllDepartments();
-            break;
-        case viewAllRolesChoice:
-            viewAllRoles();
-            break;
-        case viewAllEmployeesChoice:
-            viewAllEmployees();
-            break;
-        case addADepartmentChoice:
-            addADepartment();
-            break;
-        case addARoleChoice:
-            addARole();
-            break;
-        case addAnEmployeeChoice:
-            addAnEmployee();
-            break;
-        case updateEmployeeRoleChoice:
-            updateEmployeeRole();
-            break;
+    function handlePromptResponse(choice) {
+        switch (choice) {
+            case viewAllDepartmentsChoice:
+                viewAllDepartments();
+                break;
+            case viewAllRolesChoice:
+                viewAllRoles();
+                break;
+            case viewAllEmployeesChoice:
+                viewAllEmployees();
+                break;
+            case addADepartmentChoice:
+                addADepartment();
+                break;
+            case addARoleChoice:
+                addARole();
+                break;
+            case addAnEmployeeChoice:
+                addAnEmployee();
+                break;
+            case updateEmployeeRoleChoice:
+                updateEmployeeRole();
+                break;
+        }
     }
-}
 
-function viewAllDepartments() {
-    logMessage(viewAllDepartmentsChoice + " Selected.");
-    const query = 'SELECT * FROM department';
-    executeDisplayQuery(query, promptMenu());
-}
+    function viewAllDepartments() {
+        logMessage(viewAllDepartmentsChoice + " Selected.");
+        const query = 'SELECT * FROM department';
+        executeDisplayQuery(query, promptMenu());
+    }
 
-function viewAllRoles() {
-    logMessage(viewAllRolesChoice + " Selected.");
-    const query = 'SELECT * FROM role';
-    executeDisplayQuery(query, promptMenu());
-}
+    function viewAllRoles() {
+        logMessage(viewAllRolesChoice + " Selected.");
+        const query = 'SELECT * FROM role';
+        executeDisplayQuery(query, promptMenu());
+    }
 
-function viewAllEmployees() {
-    logMessage(viewAllEmployeesChoice + " Selected.");
-    const query = 'SELECT * FROM employee';
-    executeDisplayQuery(query, promptMenu());
-}
+    function viewAllEmployees() {
+        logMessage(viewAllEmployeesChoice + " Selected.");
+        const query = 'SELECT * FROM employee';
+        executeDisplayQuery(query, promptMenu());
+    }
 
-function addADepartment() {
-    logMessage(addADepartmentChoice + " Selected.");
-    inquirer
-        .prompt({ type: "input", name: "department", message: "Enter New Department Name: " })
-        .then((response) => {
-            const query = `INSERT INTO department(name) VALUES ('${response.department}')`;
-            executeQuery(query, promptMenu());
-        })
-        .catch((err) => console.error("There was an error: " + err));
-}
+    function addADepartment() {
+        logMessage(addADepartmentChoice + " Selected.");
+        inquirer
+            .prompt({ type: "input", name: "department", message: "Enter New Department Name: " })
+            .then((response) => {
+                query(`INSERT INTO department(name) VALUES (?)`, [response.department]);
+                promptMenu();
+                // executeQuery(query, promptMenu());
+            }) 
+            .catch((err) => console.error("There was an error: " + err));
 
-function addARole() {
-    logMessage(addARoleChoice + " Selected.");
-    //name, salary, and department
-    inquirer
-        .prompt(addARoleQuestions)
-        .then((response) => {
-            const query = `SELECT id INTO @deptid FROM department WHERE name = '${response.department}'; SELECT @deptid; INSERT INTO role(title, salary, department_id) VALUES ('${response.role}', '${response.salary}', @deptid)`;
-            executeQuery(query, promptMenu())
-        })
-        .catch((err) => console.error("There was an error: " + err));
-}
+    }
 
-function addAnEmployee() {
-    logMessage(addAnEmployeeChoice + " Selected.");
-    //name, salary, and department
-    inquirer
-        .prompt(addAnEmployeeQuestions)
-        .then((response) => {
-            const query = `SELECT id INTO @roleid FROM role WHERE title = '${response.role}'; SELECT @roleid; SELECT id INTO @managerid FROM employee WHERE first_name = '${response.manager}'; SELECT @deptid; INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES ('${response.firstName}', '${response.lastName}', @roleid, @managerid)`;
-            executeQuery(query, promptMenu())
-        })
-        .catch((err) => console.error("There was an error: " + err));
-}
+    function addARole() {
+        logMessage(addARoleChoice + " Selected.");
+        //name, salary, and department
+        inquirer
+            .prompt(addARoleQuestions)
+            .then((response) => {
+                const departmentArray = query(`SELECT id FROM department WHERE name = ?`, [response.department]);
+                const deptid = departmentArray[0].id;
+                query(`INSERT INTO role(title, salary, department_id) VALUES (?,?,?)`, [response.role, response.salary, deptid]);
+                // executeQuery(query, promptMenu())
+            })
+            .catch((err) => console.error("There was an error: " + err));
+    }
 
-async function updateEmployeeRole() {
-    logMessage(updateEmployeeRoleChoice + " Selected.");
-    const allEmployees=await sqlConnection.promise().query("select id, first_name from employee")
-    const allRoles=await sqlConnection.promise().query("select id, title from role")
-   
-    const response=await inquirer.prompt([{ type: "list", name: "empid", message: "Select Employee: ", choices: allEmployees[0].map((employee)=> ({name: employee.first_name, value: employee.id}))}, 
-                     { type: "list", name: "role", message: "Select Employee's new role: ", choices: allRoles[0].map((role)=>({name: role.title, value: role.id}))}])
+    async function addAnEmployee() {
+        logMessage(addAnEmployeeChoice + " Selected.");
+        //name, salary, and department
+        const response= await inquirer.prompt(addAnEmployeeQuestions)
+                const roleArray = await query(`SELECT id FROM role WHERE title = ?`, [response.role]);
+                console.log(roleArray);
+                const roleid = roleArray[0].id;
+                const managerArray = await query(`SELECT id FROM employee WHERE first_name = ?`, [response.manager]);
+                const managerid = managerArray[0].id;
+                await query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`, [response.firstName, response.lastName, roleid, managerid]);
+                // executeQuery(query, promptMenu())
+        promptMenu();
+    }
+
+    async function updateEmployeeRole() {
+        logMessage(updateEmployeeRoleChoice + " Selected.");
+        const allEmployees = await sqlConnection.promise().query("select id, first_name from employee")
+        const allRoles = await sqlConnection.promise().query("select id, title from role")
+        const response = await inquirer.prompt([{ type: "list", name: "empid", message: "Select Employee: ", choices: allEmployees[0].map((employee) => ({ name: employee.first_name, value: employee.id })) },
+        { type: "list", name: "role", message: "Select Employee's new role: ", choices: allRoles[0].map((role) => ({ name: role.title, value: role.id })) }])
         // executeQuery(query, promptMenu())
         sqlConnection.promise().query("UPDATE employee set role_id = ? WHERE id = ?", [response.role, response.empid]);
         promptMenu();
-}
-
-// - Helper Functions
-function logMessage(str) {
-    if (isDebug) {
-        console.log(str);
     }
-}
 
-// Executes a query and then runs the closure after it's successful.
-function executeQuery(query, closure) {
-    sqlConnection.execute(query, (err, result) => {
-        if (err) {
-            logMessage(err);
-        } else {
-            logMessage(result);
-            if (closure != null) { closure(); }
-        };
-    });
-}
+    //Helper Functions
+    function logMessage(str) {
+        if (isDebug) {
+            console.log(str);
+        }
+    }
 
-function executeDisplayQuery(query, closure) {
-    sqlConnection.execute(query, (err, result) => {
-        if (err) {
-            logMessage(err);
-        } else {
-            console.table(result);
-            if (closure != null) { closure(); }
-        };
-    });
-}
+    // Executes a query and then runs the closure after it's successful.
+    function executeQuery(query, closure) {
+        sqlConnection.execute(query, (err, result) => {
+            if (err) {
+                logMessage(err);
+            } else {
+                logMessage(result);
+                if (closure != null) { closure(); }
+            };
+        });
+    };
 
-// - Program Start
-function initializeDatabase() {
-    // This connection requires a MySQL local instance to be running.
-    sqlConnection = mysql.createConnection({
-        host: host,
-        user: username,
-        port: port,
-        password: 'Idasftw1',
-        database: "employeetracker_db"
-    });
-    sqlConnection.connect(err => {
-        if (err) {
-            logMessage(err);
-        } else {
-            logMessage("MySQL connection started.");
-            promptMenu();
+    function executeDisplayQuery(query, closure) {
+        sqlConnection.execute(query, (err, result) => {
+            if (err) {
+                logMessage(err);
+            } else {
+                console.table(result);
+                if (closure != null) { closure(); }
+            };
+        });
+    }
 
-        };
-    });
-    
-}
-
-initializeDatabase();
